0b4ec0da6f0ee7dfc011e7e7ba984fb26ec2dccb
syum-akhlaque <arssyum@gmail.com>
2020-09-29 14:00:09 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig ={
+  apiKey: "AIzaSyAwi0GCfkZaLjpGqYDu6lLNoUkD_ZEqeJY",
+  authDomain: "burj-al-arab-20.firebaseapp.com",
+  databaseURL: "https://burj-al-arab-20.firebaseio.com",
+  projectId: "burj-al-arab-20",
+  storageBucket: "burj-al-arab-20.appspot.com",
+  messagingSenderId: "238916763993",
+  appId: "1:238916763993:web:702a4ca0a7127e88520c48"
+};
+  export default firebaseConfig;
